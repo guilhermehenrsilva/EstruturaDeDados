df9b2e88d438c84e764be812f6d0bb004cfe9cf2
Guilherme Henrique <101257874+guilhermehenrsilva@users.noreply.github.com>
2023-03-14 23:15:00 -0300

    Add files via upload

---
+++ javapallet/src/javapalletscontrol/JavaPallet1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapalletscontrol;
+
+/**
+ *
+ * @author mefma
+ */
+public class JavaPallet1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormSistema1 f = new FormSistema1();
+        f.setVisible(true);
+    }
+    
+}
