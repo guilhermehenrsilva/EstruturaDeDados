b448574eb0c480d602d3189a52952c91e0310282
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2023-05-29 09:21:42 -0300

    Atualizado 29/05/2023

---
+++ Exercicios/RECURSIVIDADE.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void infinityDream(int n){
+	printf("Chamada %d: ",n);
+	if (n==0)
+		return;
+	printf("Sonhando.....\n");getch();
+	Sleep(200);
+	infinityDream(n-1);
+	printf("Retorno instancia %d\n",n);getch();
+}
+
+
+int main(){
+	int x;
+	printf("Qtd sonhos: ");
+	scanf("%d",&x);
+	infinityDream(x);
+}
